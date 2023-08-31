754e4a0800d99fed20b6fd1e726b9cc3c2d7bb68
Jose Ramon <jose.ramon.severo.alves@ccc.ufcg.edu.br>
2023-08-31 11:47:06 -0300

    Classe compra e bordados parcialmente feitas

--- BEP.java
+++ BEP.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class BEP{
         public static void main(String[] args) {
-                
+
+                String[][] matriz = new String[8][8];
+
+                System.out.println(Arrays.toString(matriz[0]));
+
+                for (int i = 0; i < 8; i++) {
+
+                        for (int j = 0; j < 8; j++) {
+                                
+                                matriz[i][j] = "" + i + "";
+
+                        }
+
+                        System.out.println(Arrays.toString(matriz[i]));
+                }
+
         }
 }
